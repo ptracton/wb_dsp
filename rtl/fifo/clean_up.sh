50584ff8f7e0fd44ec5338e92dad2fdf17a98892
Phil Tracton <ptracton@gmail.com>
2018-07-03 19:50:41 -0700

    first draft done

--- rtl/fifo/clean_up.sh
+++ rtl/fifo/clean_up.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env sh
 
-rm -rf *.vcd *~  a.out vsim.wlf work/ transcript auto *.tex _minted-fifo/ xrun.history xrun.log  xcelium.d/ 
+rm -rf *.vcd *~  a.out vsim.wlf work/ transcript auto *.tex _minted-fifo/ xrun.history xrun.log  xcelium.d/ fifo.tex fifo.pygmented _minted-fifo/ auto/
